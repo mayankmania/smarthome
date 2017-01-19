c39f0fcdbf638c944e04c4421315f345864246ca
mayankmania <mayank_p_singh@hotmail.com>
2017-01-20 00:24:31 +0530

    Added gitignore for node

---
+++ index.js
@@ -0,0 +1,82 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var gpioInstance = require('ms-gpio');
+//Define gpioPath to simulate raspberry on local machine
+var gpioPath;
+var app = new express();
+
+startUp();
+
+//Listen for all the request
+var server = app.listen(app.get('port'), function () {
+    var port = server.address().port;
+    console.log('Server running on port ' + port);
+});
+
+//Register all the startup related stuffs in this function
+function startUp() {
+    gpioPath = "GPIOTest";
+    configureExternalModule();
+    setUpHttpHandler();
+    app.set('port', 9000);
+}
+
+//Configure external modules here
+function configureExternalModule() {
+    app.use(bodyParser.urlencoded({
+        extended: true
+    }));
+
+    app.use(bodyParser.json());
+
+    var options = {
+        index: "index.htm"
+    };
+
+    app.use('/', express.static('public', options));
+}
+
+//Configure http request handler
+function setUpHttpHandler() {
+    app.use('/getDevices', function (req, res) {
+
+        // List of registerd devices, ideally should come from a database
+        var devices = [
+{ deviceId: 15, status: 0, device: "fan" },
+{ deviceId: 16, status: 0, device: "bulb" },
+{ deviceId: 18, status: 0, device: "washer" },
+{ deviceId: 19, status: 0, device: "tv" }
+        ];
+
+        for (var i = 0; i < devices.length; i++) {
+            var gpio = new gpioInstance(gpioPath);
+            var status = gpio.read(devices[i].deviceId);
+            if (status == -1) {
+                status = 0;
+            }
+            devices[i].status = status;
+        }
+        res.json(devices);
+    });
+
+    app.post("/", function (req, res) {
+        var deviceId = req.body.deviceId;
+        var gpio = new gpioInstance(gpioPath);
+        gpio.setUp(deviceId, "out");
+        var currentLEDStatus = gpio.read(deviceId);
+        if (currentLEDStatus == 0 || currentLEDStatus == -1) {
+            setApplianceState(deviceId, 1, res);
+        }
+        else {
+            setApplianceState(deviceId, 0, res);
+        }
+    });
+}
+
+//Set appliance state
+function setApplianceState(pinNo, setState, response) {
+    var gpio = new gpioInstance(gpioPath);
+    gpio.write(pinNo, setState);
+    var jsonResult = { "status": setState, "deviceId": pinNo };
+    response.json(jsonResult);
+}
---
+++ public/script/deviceManager.js
@@ -0,0 +1,30 @@
+$(document).ready(function () {
+    $.ajaxSetup({ cache: false });
+    getDevices();
+});
+
+function getDevices() {
+    $.get("/getDevices")
+    .done(function (result) {
+        for (var i = 0; i < result.length; i++) {
+            $("table tbody").append("<tr><th scope='row'>" + (i + 1) + "</th><td><img src='css/icons/" + result[i].device + ".png'" + " class='thumbnail' alt='...'></td><td><a href='javascript:void(0);' onclick='manageDevice(" + result[i].deviceId + ")'><img class='thumbnail' src='css/icons/stop.png' id='device" + result[i].deviceId + "'/> </a></td></tr>");
+            setStateIcon(result[i]);
+        }
+    });
+}
+
+function manageDevice(deviceId) {
+    $.post("/", { "deviceId": deviceId })
+    .done(function (result) {
+        setStateIcon(result);
+    });
+}
+
+function setStateIcon(result) {
+    if (result.status == 0) {
+        $("#device" + result.deviceId).attr('src', 'css/icons/start.png');
+    }
+    else if (result.status == 1) {
+        $("#device" + result.deviceId).attr('src', 'css/icons/stop.png');
+    }
+}
